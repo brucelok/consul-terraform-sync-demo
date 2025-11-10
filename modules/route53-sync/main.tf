c639ce81be41fd949a762f65bef2fa506ffef4cd
Bruce L. <2253713+brucelok@users.noreply.github.com>
2025-11-10 14:00:49 +1100

    Update AWS provider version and clean up variables
    Updated AWS provider version and removed unused variables.

--- modules/route53-sync/main.tf
+++ modules/route53-sync/main.tf
@@ -2,23 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.14.0"
+      version = "~> 6.0"
     }
   }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
-}
-
-variable "hosted_zone_id" {
-  type        = string
-  #default     = "xxxx"
-}
-
-variable "dns_name" {
-  type        = string
-  default     = "api.bruce-lok.sbx.hashidemos.io"
 }
 
 resource "aws_route53_record" "api" {
@@ -28,27 +18,4 @@ resource "aws_route53_record" "api" {
   type    = "A"
   ttl     = 300 
   records = [for s in values(var.services) : s.address]
-}
-
-# example from https://github.com/hashicorp/consul-terraform-sync/blob/main/examples/runnable-example/example-module/variables.tf
-variable "services" {
-  type = map(object({
-    id                    = string
-    name                  = string
-    kind                  = string
-    address               = string
-    port                  = number
-    meta                  = map(string)
-    tags                  = list(string)
-    namespace             = string
-    status                = string
-    node                  = string
-    node_id               = string
-    node_address          = string
-    node_datacenter       = string
-    node_tagged_addresses = map(string)
-    node_meta             = map(string)
-    cts_user_defined_meta = map(string)
-  }))
-  description = "Map of Consul service instances (injected by CTS)"
 }
